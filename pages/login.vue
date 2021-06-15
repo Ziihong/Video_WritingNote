aad5031120d24996355a4ee7bad971fbc00d6af1
dlrhkdgh0144 <dlrhkdgh0144@gmail.com>
2021-06-15 17:59:35 +0900

    Revise login page style

--- pages/login.vue
+++ pages/login.vue
@@ -1,19 +1,30 @@
 <template>
   <v-row>
     <v-col class="text-center">
-        <div class="login-block">
+        <form class="login-block">
           <input class="input_interface" placeholder="ID" />
           <br>
-          <input class="input_interface" placeholder="Password" />
+          <input class="input_interface" placeholder="Password" type="password" />
           <br>
           <v-btn
+            class="btn"
             color="primary"
             nuxt
             to="/login"
           >
             Login
           </v-btn>
-        </div>
+          <br>
+          <p>Doesn't have Account Yet?</p>
+          <v-btn
+            class="btn"
+            color="primary"
+            nuxt
+            to="/login"
+          >
+            Register
+          </v-btn>
+        </form>
     </v-col>
   </v-row>
 </template>
@@ -26,11 +37,20 @@ export default {
 
 <style scoped>
 .login-block{
-  display: block;
+  display: flex;
+  flex-direction: column;
+  margin-left: auto;
+  margin-right: auto;
 }
 .input_interface{
   background-color: white;
-  margin-top: 10px;
-  margin-bottom: 10px;
+  width : 300px;
+  margin-left: auto;
+  margin-right: auto;
+}
+.btn{
+  margin-left: auto;
+  margin-right: auto;
+  width : 200px;
 }
 </style>
