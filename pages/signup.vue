5b52cb4b2b2ee96ce25970afb7e0c34104500980
Gimyoo <5012s@kookmin.ac.kr>
2021-06-24 16:22:34 +0900

    sign up

---
+++ pages/signup.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <div>
+        <h2>회원가입</h2>
+        <input v-model="formData.email"
+               type="email"
+               autocomplete="username"
+               placeholder="이메일주소"
+               class="input"><br>
+        <input v-model="formData.password"
+               type="password"
+               autocomplete="current-password"
+               placeholder="비밀번호"
+               class="input"><br>
+        <v-btn
+          @click="createUser"
+          color="primary"
+        >
+          시작하기
+        </v-btn><br>
+      </div>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    formData: {
+      email:'',
+      password:'',
+    },
+  }),
+  methods:{
+    async createUser() {
+      try {
+        await this.$fire.auth.createUserWithEmailAndPassword(
+          this.formData.email,
+          this.formData.password
+        )
+        location.href="/login"
+      } catch (e) {
+        alert(e)
+      }
+    },
+  },
+}
+</script>
+
+<style scoped>
+.input {
+  background-color: aliceblue;
+  margin-bottom: 10px;
+}
+</style>
