17d236df91d89cbb5b249550a3e3a4784bf9534a
dlrhkdgh0144 <dlrhkdgh0144@gmail.com>
2021-06-28 11:32:03 +0900

    Test video capture(on progress)

--- pages/mypage.vue
+++ pages/mypage.vue
@@ -15,16 +15,21 @@
       </v-row>
       <v-row>
         <video class="video-frame" controls autoplay muted
-               src="/hd.mp4"
+               src="/hd.mp4" id="videoOrigin"
                >
           브라우저가 비디오 플레이를 지원하지 않습니다
         </video>
+
+      </v-row>
+      <v-row id="draw">
+        <p>canvas below here</p>
+        <canvas id="videoCanvas"></canvas>
       </v-row>
     </v-col>
     <v-col cols="4" class="comment-box">
       <v-row>
         <v-btn color="primary" @click="choiceFile">새파일</v-btn>
-        <v-btn color="primary">test2</v-btn>
+        <v-btn color="primary" @click="drawVideo">test2</v-btn>
         <v-btn color="primary">test3</v-btn>
       </v-row>
       <v-row>
@@ -46,10 +51,33 @@
 
 <script>
 
+import html2canvas from "html2canvas";
+
 export default {
   methods: {
     choiceFile: function (){
       document.getElementById("fileupload").click();
+    },
+    drawVideo: function (){
+      this.video = document.querySelector("#videoOrigin");
+      this.canvas = document.querySelector("#videoCanvas");
+      this.context = this.canvas.getContext('2d');
+
+      this.canvas.width = this.video.clientWidth;
+      this.canvas.height = this.video.clientHeight;
+
+      this.context.drawImage(this.video, 0, 0, this.video.width, this.video.height);
+      console.log(this.canvas);
+      console.log(this.context.getImageData(0,0,this.canvas.clientWidth,this.canvas.clientHeight));
+      /*
+      const video = document.querySelector("#videoOrigin");
+      html2canvas(video).then(canvas =>{
+        let a = document.createElement("a");
+        a.href = canvas.toDataURL("image/jpeg").replace("image/jpeg","image/octet-stream");
+        a.download = "out.jpg";
+        a.click();
+        document.querySelector("#draw").appendChild(canvas)
+      });*/
     }
   }
 }
@@ -63,5 +91,6 @@ export default {
 .video-frame{
   max-width: 100%;
   height: auto;
+
 }
 </style>
