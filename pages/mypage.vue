6a401ebc0936596551f0f8dfc876127375d1bb49
leeinho <inhoking@kookmin.ac.kr>
2021-06-29 15:16:53 +0900

    add video mark

--- pages/mypage.vue
+++ pages/mypage.vue
@@ -11,33 +11,19 @@
           </v-icon>
           내 파일함</v-btn>
         <input placeholder="제목을 입력하세요" type="text"
-        style="width: 40%; background-color:white;"/>
+               style="width: 40%; background-color:white;"/>
       </v-row>
       <v-row>
-<!--        <video ref="video" class="video-frame" controls autoplay muted-->
-<!--               src="/hd.mp4" id="video"-->
-<!--               >-->
+        <video width="100%" height="500" controls id="player" src="/hd.mp4">
+          브라우저가 비디오 플레이를 지원하지 않습니다
+        </video>
+        <div id="marker">
+
+        </div>
+<!--        <video id="myVideo" ref="player" class="video-js vjs-big-play-centered vjs vjs-fluid" data-setup="{}">-->
+<!--          <source src="/hd.mp4" type="video/mp4">-->
 <!--          브라우저가 비디오 플레이를 지원하지 않습니다-->
 <!--        </video>-->
-        <video-player  class="video-player-box"
-                       ref="videoPlayer"
-                       :options="playerOptions"
-                       :playsinline="true"
-                       customEventName="customstatechangedeventname"
-
-                       @play="onPlayerPlay($event)"
-                       @pause="onPlayerPause($event)"
-                       @ended="onPlayerEnded($event)"
-                       @waiting="onPlayerWaiting($event)"
-                       @playing="onPlayerPlaying($event)"
-                       @loadeddata="onPlayerLoadeddata($event)"
-                       @timeupdate="onPlayerTimeupdate($event)"
-                       @canplay="onPlayerCanplay($event)"
-                       @canplaythrough="onPlayerCanplaythrough($event)"
-
-                       @statechanged="playerStateChanged($event)"
-                       @ready="playerReadied">
-        </video-player>
       </v-row>
     </v-col>
     <v-col cols="4" class="comment-box">
@@ -49,16 +35,18 @@
       <v-row>
         <v-btn>노트</v-btn>
         <v-btn>코멘트</v-btn>
-        <v-btn @click="capture">캡쳐</v-btn>
       </v-row>
       <v-row>
         <textarea wrap="virtual" rows="8" cols="40"
                   style="height: 450px"
                   placeholder="내용을 입력하세요"
+                  ref="textareaObj"
+                  id="textBox"
         ></textarea>
       </v-row>
       <v-row>
-        <v-btn color="primary" @click="onTimeUpDate">save</v-btn>
+        <v-btn color="primary">save</v-btn>
+        <v-btn color="primary" @click="makeMarker">Mark</v-btn>
       </v-row>
     </v-col>
   </v-row>
@@ -66,70 +54,35 @@
 
 <script>
 
-import 'videojs-markers'
-import 'video.js/dist/video-js.css'
-import {videoPlayer} from 'vue-video-player'
-
 export default {
-  components: {
-    videoPlayer
-  },
-  data: {
-    return: {
-      playerOptions: {
-        muted: true,
-        langueage: 'en',
-        playbackRate: [0.7, 1.0, 1.5, 2.0],
-        sources: [{
-          type: "video/mp4",
-          src: "/hd.mp4"
-        }],
-      }
-    }
-    // currentTime: 0,
-    // player: null
+
+  data() {
+    return {
+    };
   },
   mounted() {
-    this.player= videojs(this.$refs.video, this.options, function onPlayerReady() {
-      console.log('onPlayerReady', this);
-    })
-  },
-  beforeDestroy() {
-    if (this.player) {
-      this.player.dispose()
-    }
+    console.log(this.player)
+    // console.log('this is current player instance object', this.player)
   },
   methods: {
     choiceFile: function (){
       document.getElementById("fileupload").click();
     },
-    onTimeUpDate: function() {
-      this.currentTime = this.$refs.video.currentTime;
-      this.player = videojs('video')
-      this.player.markers({
-        markers:[
-          {time: 1},
-          {time: 3}
-        ]
-      })
-    },
-
-    // screenShot: function() {
-    //   html2canvas(this.$refs.video, {
-    //     useCORS: true
-    //   }).then((canvas) => {
-    //     this.fileDownload(canvas.toDataURL(), "capture.png")
-    //   })
-    // },
-    // fileDownload(downloadUrl, downloadName) {
-    //   let aLink = document.createElement("a");
-    //   aLink.href = downloadUrl;
-    //   aLink.download = downloadName;
-    //   document.body.appendChild(aLink);
-    //   aLink.click();
-    //   document.body.removeChild(aLink);
-    // }
-
+    makeMarker: function(){
+      let tmp = document.getElementById("player");
+      let value = this.$refs.textareaObj.value;
+      let time = tmp.currentTime;
+      let newBtn = document.createElement('button');
+      let txt = document.createTextNode("000");
+      newBtn.style.backgroundColor="red";
+      newBtn.style.borderRadius = '50%';
+      newBtn.appendChild(txt);
+      document.getElementById('marker').appendChild(newBtn);
+      newBtn.addEventListener('click', function(){
+        tmp.currentTime = time;
+        document.getElementById('textBox').value = value;
+      });
+    }
   }
 }
 </script>
