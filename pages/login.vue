27a721c7a0e3f0bfc190f0527f2b410672bef1ea
junseokkim <jsutin639@gmail.com>
2021-06-30 10:57:11 +0900

    [T2] Pull Team2

--- pages/login.vue
+++ pages/login.vue
@@ -1,116 +1,106 @@
 <template>
-  <v-card style="width:320px; top:100px;" class="mx-auto">
-    <v-card-title>
-      로그인
-    </v-card-title>
-
-    <v-card-text>
-      <v-form>
-        <v-text-field v-model="form.email" label="이메일 주소"></v-text-field>
-        <v-text-field
-          v-model="form.password"
-          label="비밀번호"
-          type="password"
-          :append-icon="form.show1 ? 'mdi-eye' : 'mdi-eye-off'"
-          :rules="[form.rules.required, form.rules.min]"
-          :type="form.show1 ? 'text' : 'password'"
-          hint="At least 8 characters"
-          couter
-          @click:append="form.show1 = !form.show1">
-        </v-text-field>
-      </v-form>
-    </v-card-text>
-
-    <v-card-actions>
-      <v-btn class="blue white--text" style="width:100%" @click="signIn">
-        로그인
-      </v-btn>
-    </v-card-actions><br/>
-
-    <v-card-actions>
-      <v-btn class="white black--text" style="width:100%" @click="googleLogin">
-        <v-icon left>fab fa-google</v-icon>
-        구글 계정으로 로그인
-      </v-btn>
-    </v-card-actions>
-
-    <v-card-actions>
-      <v-container>
-        <v-row justify="center">
-          <v-col cols="auto">
-            <v-btn class="blue--text" style="background-color: transparent" @click="signUp">회원가입</v-btn>
-          </v-col>
-          <v-spacer></v-spacer>
-          <v-col cols="auto">
-            <v-btn class="blue--text" style="background-color: transparent">비밀번호 찾기</v-btn>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-card-actions>
-
-  </v-card>
+  <Auth/>
 </template>
 
 <script>
-import firebase from "firebase";
-
+import Auth from '~/components/Auth';
 export default {
   layout: 'empty',
-  data() {
-    return {
-      form: {
-        email: '',
-        password: '',
-        show1: false,
-        rules: {
-          required: value => !!value || 'Required.',
-          min: v => v.length >= 8 || 'Min 8 characters',
-          email: value => {
-            const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-            return pattern.test(value) || 'Invalid e-mail.'
-          }
-        }
-      }
-    }
-  },
-
-  methods: {
-    async signIn() {
-      try {
-        const r = await this.$fire.auth.signInWithEmailAndPassword(
-          this.form.email,
-          this.form.password
-        )
-        await this.$router.push('/')
-      } catch (e) {
-        alert(e.message)
-      }
-    },
-
-    async signUp() {
-      await this.$router.push('/auth')
-    },
-
-    async googleLogin() {
-      try {
-        const provider = new firebase.auth.GoogleAuthProvider()
-        this.$fire.auth.languageCode = 'Korean'
-        const r = await this.$fire.auth.signInWithPopup(provider)
-        this.$store.commit('setUser', r.user)
-        await this.$router.push('/')
-      } catch (e) {
-        console.error(e.message)
-      }
-    },
-
-    async reqNormal() {
-      const tk = await this.$fire.auth.currentUser.getIdToken()
-      this.$axios.setToken(tk)
-      const data = await this.$axios.get(
-        'http://localhost:3000/login'
-      )
-      console.log(data)
-    }
+  computed:{
+    Auth
   }
 }
 </script>
+
+<style scoped>
+template{
+  width: 35%;
+}
+</style>
+
+<!--<template>-->
+<!--  <div class="logIn">-->
+<!--    <h3>Log In</h3><br>-->
+<!--    <v-form v-if="!isLoggedIn" onsubmit="return false;">-->
+<!--      <span>Email  </span>-->
+<!--      <v-text-field-->
+<!--        v-model="formData.email"-->
+<!--        type="text"-->
+<!--        placeholder="email">-->
+<!--      </v-text-field>-->
+<!--      <span>PassWord  </span>-->
+<!--      <v-text-field-->
+<!--        v-model="formData.password"-->
+<!--        type="password"-->
+<!--        placeholder="password">-->
+<!--      </v-text-field>-->
+<!--      <v-btn @click="signInUser">Log In</v-btn><br>-->
+<!--      <v-btn><router-link to="/signUp">sign Up</router-link></v-btn>-->
+<!--    </v-form>-->
+<!--    <div v-else>-->
+<!--      <p>Hello! {{ authUser.displayName }}.</p>-->
+<!--      <v-btn color="primary" outlined @click="logout">Logout</v-btn>-->
+<!--    </div>-->
+<!--</template>-->
+
+<!--<script>-->
+
+<!--import {mapGetters, mapState} from "vuex";-->
+
+<!--export default {-->
+<!--  name: 'logIn',-->
+<!--  data: () => ({-->
+<!--    formData: {-->
+<!--      email: '',-->
+<!--      password: ''-->
+<!--    },-->
+<!--  }),-->
+<!--  computed: {-->
+<!--    ...mapState({-->
+<!--      authUser: state => state.authUser,-->
+<!--    }),-->
+<!--    ...mapGetters({-->
+<!--      isLoggedIn: 'isLoggedIn',-->
+<!--    }),-->
+<!--  },-->
+<!--  methods: {-->
+<!--    async signInUser() {-->
+<!--      try {-->
+<!--        await this.$fire.auth.signInWithEmailAndPassword(-->
+<!--          this.formData.email,-->
+<!--          this.formData.password-->
+<!--        )-->
+<!--      } catch (e) {-->
+<!--        alert(e)-->
+<!--      }-->
+<!--    },-->
+<!--    async logout() {-->
+<!--      try {-->
+<!--        await this.$fire.auth.signOut()-->
+<!--      } catch (e) {-->
+<!--        alert(e)-->
+<!--      }-->
+<!--    },-->
+<!--  }-->
+<!--}-->
+<!--</script>-->
+<!--<style scoped>-->
+<!--  .logIn{-->
+<!--    margin-top: 40px;-->
+<!--  }-->
+<!--  .logInForm{-->
+<!--    margin: 0 auto;-->
+<!--  }-->
+<!--  input{-->
+<!--    margin: 10px 0;-->
+<!--    width: 20%;-->
+<!--    padding:15px;-->
+<!--    color: white;-->
+<!--  }-->
+<!--  button{-->
+<!--    margin-top: 30px;-->
+<!--    width: 10%;-->
+<!--    cursor: pointer;-->
+<!--  }-->
+<!--</style>-->
+
