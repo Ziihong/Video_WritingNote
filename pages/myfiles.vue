2865aaa60abe72c7dbfe8670fdb217d45f4fdb6d
Gimyoo <5012s@kookmin.ac.kr>
2021-07-02 14:29:16 +0900

    Feat: Create new directory and file storage

--- pages/myfiles.vue
+++ pages/myfiles.vue
@@ -1,65 +1,91 @@
 <template>
-  <div>
+  <v-col>
+    <!-- directory path -->
     <v-col>
-      <v-row>
-        <div>
-          path: {{ path }}
+      <span v-for="path of pathIds"> > <v-btn @click="movePath(path)"> {{ path.pathName }} </v-btn> </span>
+    </v-col>
+
+    <!-- buttons container -->
+    <v-col>
+      <!-- button : make new directory -->
+      <v-row class="btnContainer">
+        <div class="btn">
+          <v-btn color="primary" class="filebox" @click="makeDirectory">
+            <v-icon left>mdi-folder-plus-outline</v-icon>
+            새폴더
+          </v-btn>
+          <v-text-field clearable placeholder="새폴더 이름을 입력하세요" v-model="newName"/>
         </div>
       </v-row>
-      <v-row>
-    <form>
-      <!-- upload directory button -->
-      <v-btn color="primary" class="filebox">
-        <label for="up-directory">
-          <v-icon left>mdi-folder-upload-outline</v-icon>
-          폴더 업로드
-        </label>
-        <v-file-input class="selectFile" id="up-directory" @change="onSelectDir" v-model="name" light webkitdirectory
-                      directory multiple/>
-      </v-btn>
-      <!-- upload file button -->
-      <v-btn color="primary" class="filebox">
-        <label for="up-file">
-          <v-icon left>mdi-file-upload-outline</v-icon>
-          파일 업로드
-        </label>
-        <v-file-input class="selectFile" id="up-file" @change="onSelect" v-model="name" light/>
-      </v-btn>
-      <!-- make new directory -->
-      <v-btn color="primary" class="filebox" @click="newDirectory">
-        <v-icon left>mdi-folder-plus-outline</v-icon>
-        새폴더
-      </v-btn>
-      <input type="text" @change="newDirectory">
-    </form>
+
+      <!-- upload buttons container -->
+      <v-row class="btnContainer">
+        <!-- button: upload directory -->
+        <div class="btn">
+          <v-btn color="primary" class="filebox">
+            <label for="up-directory">
+              <v-icon left>mdi-folder-upload-outline</v-icon>
+              폴더 업로드
+            </label>
+            <v-file-input class="selectFile" id="up-directory" @change="upDirectory" v-model="name" light
+                          webkitdirectory
+                          directory multiple/>
+          </v-btn>
+        </div>
+        <!-- button : upload file -->
+        <div class="btn">
+          <v-btn color="primary" class="filebox">
+            <label for="up-file">
+              <v-icon left>mdi-file-upload-outline</v-icon>
+              파일 업로드
+            </label>
+            <v-file-input class="selectFile" id="up-file" @change="upFile" v-model="name" light/>
+          </v-btn>
+        </div>
       </v-row>
-    <!-- View Directories -->
-      <v-row>
-    <template v-for="dir of directories">
-      <div class="directory">
-        <v-icon left>mdi-folder</v-icon>
-        <span @click="moveDir(dir)">{{ dir.data().name }}</span>
-        <button @click="removeDirectory(dir)">
-          <v-icon right>mdi-close-box</v-icon>
-        </button>
-      </div>
-    </template>
-      </v-row>
-    <!-- View Files -->
-      <v-col>
-    <template v-for="file of files">
-      <li>
-        <span @click="movePage">{{ file.data().name }}</span>
-        <button @click="removeFile(file)">
-          <v-icon right>mdi-close-box</v-icon>
-        </button>
-      </li>
-    </template>
-        </v-col>
-    <!-- function test button -->
     </v-col>
-    <v-btn color="primary" @click="getKey">click</v-btn>
-  </div>
+
+    <!-- view directories -->
+    <v-col>
+      <v-row>
+        <div>폴더</div>
+      </v-row>
+      <v-row>
+        <template v-for="dir of directories" >
+          <div class="directory">
+            <div class="inlineBtn" @click="moveDirectory(dir)">
+              <v-icon left>mdi-folder</v-icon>
+              <span>{{ dir.data().name }}</span>
+            </div>
+            <button class="inlineBtn">
+              <v-icon right @click="removeDirectory(dir)">mdi-close-box</v-icon>
+            </button>
+          </div>
+        </template>
+      </v-row>
+    </v-col>
+
+    <!-- view files -->
+    <v-col>
+      <v-row>
+        <div>파일</div>
+      </v-row>
+      <v-row>
+        <template v-for="file of files">
+          <div class="directory">
+            <div class="inlineBtn">
+              <v-icon left>mdi-file</v-icon>
+              <span>{{ file.data().name }}</span>
+            </div>
+            <button class="inlineBtn">
+              <v-icon right @click="removeFile(file)">mdi-close-box</v-icon>
+            </button>
+          </div>
+        </template>
+      </v-row>
+    </v-col>
+
+  </v-col>
 </template>
 
 <script>
@@ -72,13 +98,19 @@ export default {
   data() {
     return {
       name: '',
-      files: [],
+      newName: '',
+      path: '',
       directories: [],
-      fileObj: null,
-      isUploading: false,
-      fileUrls: [],
-      path: `users/${this.$fire.auth.currentUser.uid}/files`,
-    };
+      files: [],
+      currentDirectory: '',
+      pathIds: [
+        {
+          pathId:'',
+          pathName:'내 보관함',
+          parentId:'',
+        }
+      ],
+    }
   },
   async mounted() {
     if (!this.$fire.auth.currentUser) {
@@ -92,95 +124,113 @@ export default {
         console.log('');
       }
     })
-    this.$fire.firestore.doc(this.path).collection('files').orderBy('name').onSnapshot((async querySnapshot => {
-      console.log(querySnapshot.docs.length);
-      this.files = querySnapshot.docs;
-      const self = this;
-      this.fileUrls = await Promise.all(this.files.map(file => file.data().path ? self.$fire.storage.ref(file.data().path).getDownloadURL() : ''));
-      console.log('fileUrls', this.fileUrls);
-    }));
-    this.$fire.firestore.doc(`users/${this.$fire.auth.currentUser.uid}`).collection('directories').orderBy('name').onSnapshot((async querySnapshot => {
-      //console.log(querySnapshot.docs.length);
-      this.directories = querySnapshot.docs;
-
-      const self = this;
-      this.fileUrls = await Promise.all(this.files.map(file => file.data().path ? self.$fire.storage.ref(file.data().path).getDownloadURL() : ''));
-      console.log('fileUrls', this.fileUrls);
-    }));
+    this.viewFiles();
   },
   methods: {
-    getKey(){
-      const len = 21;
-      const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-      const charactersLength = characters.length;
-      let key = '';
-      for ( let i = 0; i < len; i++ ) {
-        key += characters.charAt(Math.floor(Math.random() *
-          charactersLength));
-      }
-      return key;
+    viewFiles() {
+      // view directories
+      this.directories = [];
+      const dirStorageRef = this.$fire.firestore
+        .collection(`users/${this.$fire.auth.currentUser.uid}/directories`);
+      dirStorageRef.orderBy('name').where('parentId', '==', this.currentDirectory)
+        .onSnapshot((async querySnapshot => {
+          this.directories = querySnapshot.docs;
+        }));
+
+      // view files
+      this.files = [];
+      const fileStorageRef = this.$fire.firestore
+        .collection(`users/${this.$fire.auth.currentUser.uid}/files`);
+      fileStorageRef.orderBy('name').where('parentId', '==', this.currentDirectory)
+        .onSnapshot((async querySnapshot => {
+          //console.log(querySnapshot.docs.length);
+          this.files = querySnapshot.docs;
+          const self = this;
+          this.fileUrls = await Promise.all(this.files.map(file => file.data().path ? self.$fire.storage.ref(file.data().path).getDownloadURL() : ''));
+          console.log('fileUrls', this.fileUrls);
+        }));
     },
-    moveDir(dir){
-      this.path += `/${dir.id}`;
-      const path = this.$fire.firestore.doc(`users/${this.$fire.auth.currentUser.uid}/directories/${dir.id}`)
-      path.get().then(docSnap => {
-        if (docSnap.exists) {
-          this.name = docSnap.data().name;
-        } else {
-          console.log('');
-        }
-      })
-      // view
-      path.collection(dir.id).orderBy('name').onSnapshot((async querySnapshot => {
-        //console.log(querySnapshot.docs.length);
-        this.directories = [];
-        this.files = querySnapshot.docs;
-
-        const self = this;
-        this.fileUrls = await Promise.all(this.directories.map(dir => dir.data().path ? self.$fire.storage.ref(dir.data().path).getDownloadURL() : ''));
-        console.log('fileUrls', this.fileUrls);
-      }));
-
-      console.log('move dir');
+    async movePath(path) {
       // console.log(path);
-    },
-    movePage() {
-      this.$router.push('/mypage');
-    },
-    async newDirectory() {
-      console.log('New Dir!!!');
-      let storageRef = this.$fire.firestore.doc(`users/${this.$fire.auth.currentUser.uid}/`).collection('directories')
+      // if user clicks '내 보관함'
+      if (path.pathId == '') {
+        this.currentDirectory = '';
+        this.pathIds = [
+          {
+            pathId: '',
+            pathName: '내 보관함',
+            parentId: '',
+          }
+        ];
+        this.viewFiles();
+        return;
+      }
 
-      const docID = `newDirectory${this.directories.length + 1}`;
-      storageRef = storageRef.doc(docID)
-      storageRef.set({name: docID});
+      // if else user clicks other path
+      await this.$fire.firestore
+        .collection(`users/${this.$fire.auth.currentUser.uid}/directories`)
+        .doc(path.pathId)
+        .get().then((doc) => {
+          if (doc.exists) {
+            this.moveDirectory(doc);
+          }
+        });
+    },
+    async moveDirectory(dir) {
+      //console.log(dir.id);
+      this.currentDirectory = dir.id;
+      this.pathIds = [];
+      let tempPathIds = [];
 
-      storageRef.collection(docID).add({timestamp: this.$fireModule.firestore.FieldValue.serverTimestamp()});
+      while (true) {
+        // console.log('Start loop');
+        tempPathIds.push({pathId: dir.id, pathName: dir.data().name, parentId: dir.data().parentId});
+        let pid = dir.data().parentId;
+        if (pid == '') {
+          break;
+        }
 
+        await this.$fire.firestore
+          .collection(`users/${this.$fire.auth.currentUser.uid}/directories`)
+          .doc(pid)
+          .get().then((doc) => {
+            if (doc.exists) {
+              dir = doc;
+            }
+          });
+      }
+      // console.log('End loop');
+      tempPathIds.push({pathId: '', pathName: '내 보관함', parentId: ''});
+      for (let i = tempPathIds.length - 1; i >= 0; i--) {
+        this.pathIds.push(tempPathIds[i]);
+      }
+      this.viewFiles();
     },
-    async removeDirectory(dir) {
-      //console.log(this.$fire.firestore.doc(`users/${this.$fire.auth.currentUser.uid}/directories/${dir.id}`));
-      await this.$fire.firestore.doc(`users/${this.$fire.auth.currentUser.uid}/directories/${dir.id}`).delete();
+    async makeDirectory() {
+      if (!this.newName?.length) {
+        return alert("폴더명을 입력해주세요");
+      }
+
+      // if it is same name directory in same path, user can't make directory for newName.
+      const sameRef = await this.$fire.firestore.doc(`users/${this.$fire.auth.currentUser.uid}/`)
+        .collection('directories')
+      const querySnap = await sameRef.where("parentId", "==", this.currentDirectory).where('name', '==', this.newName).get();
+      if (querySnap.docs.length){
+        return alert("이미 존재하는 폴더명입니다. 다른 이름을 입력하세요.");
+      }
+      await this.$fire.firestore.doc(`users/${this.$fire.auth.currentUser.uid}/`).collection('directories')
+        .add({
+          name: this.newName,
+          parentId: this.currentDirectory
+        })
+      this.newName ='';
+      alert("폴더가 생성되었습니다.");
+      this.viewFiles();
     },
-    async removeFile(file) {
-      // console.log('file: ', file);
-      // console.log(file.data().name);
-      const ex = this.$fire.firestore.doc(`users/${this.$fire.auth.currentUser.uid}/files/${file.id}`);
-      await this.$fire.firestore.doc(`users/${this.$fire.auth.currentUser.uid}/files/${file.id}`).delete();
-      const storageRef = this.$fire.storage.ref(file.data().name);
-      console.log('storageRef:', storageRef);
-      storageRef.delete().then(function () {
-        console.log('delete!');
-      }).catch(function (error) {
-        alert('error');
-      });
+    upDirectory() {
+      return;
     },
-    onSelectDir(files) {
-      console.log(files.name);
-      console.log(files);
-      return 0;
-    },
-    onSelect(file) {
+    upFile(file) {
       console.log('file:', file);
       this.fileObj = file;
       console.log('fileObj:', this.fileObj);
@@ -190,6 +240,7 @@ export default {
       if (!this.fileObj) {
         return alert('파일을 선택해주세요.');
       }
+
       const storageRef = this.$fire.storage.ref(`users/${this.$fire.auth.currentUser.uid}/${this.fileObj.name}`);
       console.log('storageRef:', storageRef);
       console.log('fileObj.name:', this.fileObj.name);
@@ -220,23 +271,83 @@ export default {
         // For instance, get the download URL: https://firebasestorage.googleapis.com/...
         console.log(uploadTask.snapshot.ref.fullPath);
 
+        //const docReference
         await self.$fire.firestore.collection(`users/${self.$fire.auth.currentUser.uid}/files`).add({
+          parentId: self.currentDirectory,
           name: uploadTask.snapshot.ref.fullPath,
           path: uploadTask.snapshot.ref.fullPath,
           timestamp: self.$fireModule.firestore.FieldValue.serverTimestamp()
         })
-        this.isUploading = false;
 
         uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
           console.log('File available at', downloadURL);
         });
       });
+    },
+    async removeDirectory(dir){
+      const delConfirm = confirm("폴더와 하위 항목들을 완전히 삭제하시겠습니까?");
+      if (delConfirm == true) {
+        let subDirectories = [dir.id];
+        let subFiles = [];
+
+        // delete sub directories
+        while (subDirectories.length != 0) {
+          console.log("Start loop");
+          let currentId = subDirectories.pop();
+          // console.log("currentID:", currentId);
+          // console.log("currentList:", subDirectories);
+          await this.$fire.firestore.doc(`users/${this.$fire.auth.currentUser.uid}/directories/${currentId}`).delete();
+          const dirStorageRef = await this.$fire.firestore.doc(`users/${this.$fire.auth.currentUser.uid}/`)
+            .collection('directories');
+          await dirStorageRef.where("parentId", "==", currentId).get()
+            .then((querySnapshot) => {
+              querySnapshot.forEach((doc) => {
+                subDirectories.push(doc.id);
+                console.log(doc.id, doc.data().name);
+              });
+            });
+          const fileStorageRef = await this.$fire.firestore.doc(`users/${this.$fire.auth.currentUser.uid}/`)
+            .collection('files');
+          await fileStorageRef.where("parentId", "==", currentId).get()
+            .then((querySnapshot) => {
+              querySnapshot.forEach((doc) => {
+                subFiles.push(doc.id);
+              });
+            });
+        }
+
+        // delete sub files
+        for (let i = 0; i < subFiles.length; i++) {
+          await this.$fire.firestore.doc(`users/${this.$fire.auth.currentUser.uid}/files/${subFiles[i]}`).delete();
+        }
+        //console.log("end loop");
+        this.viewFiles();
+      }
+    },
+    async removeFile(file){
+      //console.log(file);
+      const delConfirm = confirm("파일을 완전히 삭제하시겠습니까?");
+      if (delConfirm == true){
+        await this.$fire.firestore.doc(`users/${this.$fire.auth.currentUser.uid}/files/${file.id}`).delete();
+        alert("파일이 삭제되었습니다.");
+        this.viewFiles();
+      }
     }
-  }
+
+  },
 }
+
 </script>
 
 <style scoped>
+.btn {
+  margin-right: 10px;
+}
+
+.inlineBtn {
+  display: inline;
+}
+
 .filebox label {
   display: inline-block;
   padding: .5em .75em;
@@ -256,13 +367,15 @@ export default {
   border: 0;
 }
 
-.directory{
+.btnContainer {
+  margin-bottom: 5px;
+}
+
+.directory {
   display: inline;
   padding: 10px;
-  margin:10px;
+  margin: 10px;
   border-radius: 10px;
   border: solid 2px cornflowerblue;
 }
-
-
 </style>
