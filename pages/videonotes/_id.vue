ef607ac6dd74da2f6132f3a04143096e454de522
leeinho <inhoking@kookmin.ac.kr>
2021-07-09 14:58:29 +0900

    FIX: videonotes id file changed

--- pages/videonotes/_id.vue
+++ pages/videonotes/_id.vue
@@ -2,21 +2,10 @@
   <v-row>
     <v-col cols="8">
       <v-row>
-        <div id="dimmed" class="hidden" @click="closePopup"></div>
-        <div id="canvas-container" class="hidden">
-          <Drawing @colorCall="colorChange"
-                   @modeCall="selectMode"
-                   @clearCall="canvasClear"
-                   @rangeCall="rangeChange"
-                   @undoCall="undoExec"
-                   @redoCall="redoExec"
-          ></Drawing>
-          <canvas id="drawing-canvas"
-                  @mousemove="canvasMousemove"
-                  @mousedown="canvasMousedown"
-                  @mouseleave="stopPainting"
-                  @mouseup="stopPainting"></canvas>
-        </div>
+        <Drawing :isCanvasViewed="this.isCanvasOn"
+                 :imageSrc="this.canvasImgsrc"
+                 ref="drawingPopup"
+        ></Drawing>
         <v-btn color="primary" @click="toStorage">
           <v-icon left>
             mdi-arrow-left-circle
@@ -35,10 +24,6 @@
       </v-row>
       <v-row id="draw">
         <canvas id="videoCanvas" ref="textarea" class="hidden"
-                @mousemove="canvasMousemove"
-                @mousedown="canvasMousedown"
-                @mouseleave="stopPainting"
-                @mouseup="stopPainting"
         ></canvas>
         <v-stepper v-model="el">
           <v-stepper-header>
@@ -53,7 +38,7 @@
         </v-stepper>
       </v-row>
     </v-col>
-    <v-col cols="4" class="comment-box">
+    <v-col cols="4" class="note-box">
       <v-row>
         <v-btn color="primary">공유하기</v-btn>
         <v-btn color="primary" @click="drawVideo">캡쳐</v-btn>
@@ -199,30 +184,39 @@ export default {
       }));
     },
     async onSaveNote() {
-      this.$fire.firestore.doc(`users/${this.$fire.auth.currentUser.uid}`).
-      set({name: this.name}, {merge: true}).
-      then(para => {
-        console.log('Save test : '+para);
-      });
-
-      this.noteTexts = document.getElementById('content-editor');
+      this.noteTexts = document.getElementById('content-editor').getElementsByTagName('div');
       const self = this;
-      console.log(this.noteTexts.innerText);
-      await self.$fire.firestore.collection(`users/${self.$fire.auth.currentUser.uid}/notes`).add({
-        text: self.noteTexts.innerText,
-        timestamp: self.$fireModule.firestore.FieldValue.serverTimestamp()
-      });
+      const textArr = [];
+      //add contentEditable div text because this is not in div tag
+      let strSplit = document.getElementById('content-editor').innerHTML.split('<div>');
+      if(this.notes.length === 0){
+        textArr.push(strSplit[0]);
+      }
+      for(let i=0; i<self.noteTexts.length; i++){
+        textArr.push(self.noteTexts[i].innerText);
+      }
+      //delete all that has saved before
+      let i = this.notes.length;
+      while(i !== 0){
+        await this.$fire.firestore.doc(`users/${this.$fire.auth.currentUser.uid}/notes/${this.notes[0].id}`).delete();
+        i--;
+      }
+      for(const i of textArr){
+        await self.$fire.firestore.collection(`users/${self.$fire.auth.currentUser.uid}/notes`).add({
+          text: i,
+          timestamp: self.$fireModule.firestore.FieldValue.serverTimestamp()
+        });
+      }
       this.notes = [];
       const fileStorageRef = this.$fire.firestore
         .collection(`users/${this.$fire.auth.currentUser.uid}/notes`);
       fileStorageRef.orderBy('timestamp')
         .onSnapshot((async querySnapshot => {
-          //console.log(querySnapshot.docs.length);
           this.notes = querySnapshot.docs;
           const self = this;
           this.noteUrls = await Promise.all(this.notes.map(note => note.data().path ? self.$fire.storage.ref(note.data().path).getDownloadURL() : ''));
         }));
-      this.noteTexts.innerHTML = "";
+      document.getElementById('content-editor').innerHTML = "";
     },
     async removeMark(mark) {
       await this.$fire.firestore.doc(`users/${this.$fire.auth.currentUser.uid}/marks/${mark.id}`).delete();
@@ -231,11 +225,9 @@ export default {
       this.video = document.querySelector("#videoOrigin");
       this.canvas = document.querySelector("#videoCanvas");
       this.context = this.canvas.getContext('2d');
-      this.drawcanvas = document.querySelector("#drawing-canvas");
-      this.drawcontext = this.canvas.getContext('2d');
-
       this.canvas.width = this.video.clientWidth;
       this.canvas.height = this.video.clientHeight;
+
       this.context.drawImage(this.video, 0, 0, this.canvas.width, this.canvas.height);
       const imgNode = document.createElement("img");
       imgNode.src = this.canvas.toDataURL();
@@ -243,18 +235,12 @@ export default {
       this.canvasImgsrc = this.canvas.toDataURL();
       imgNode.width = this.canvas.width/4;
       imgNode.height = this.canvas.height/4;
-      this.context.strokeStyle = this.curColor;
-      this.context.fillStyle = this.curColor;
-      this.context.lineWidth = this.brushSize;
-      this.undoStack.push(this.context.getImageData(0,0,this.canvas.width,this.canvas.height));
 
       imgNode.addEventListener("click",this.popupCanvas);
 
       document.getElementById('content-editor').appendChild(imgNode);
+      this.isCanvasOn = false;
       //document.getElementsByClassName("ProseMirror")[0].appendChild(imgNode);
-    },
-    textEdit: function (command) {
-      document.execCommand(command);
     },
     choiceFile: function () {
       document.getElementById("fileupload").click();
@@ -299,144 +285,27 @@ export default {
       }).then(function(canvas){
         let imgData = canvas.toDataURL('image/png');
         let imgWidth = 210;
-        let pageHeight = imgWidth * 1.414;
         let imgHeight = canvas.height * imgWidth / canvas.width;
-        let heightLeft =  imgHeight;
         let doc = new jsPDF('p', 'mm');
         let position = -1;
 
         doc.addImage(imgData, 'PNG', -6, position, imgWidth, imgHeight);
-
-        // heightLeft -= pageHeight;
-        // while(heightLeft >= 20){
-        //  position -= heightLeft - imgHeight;
-        //  doc.addPage();
-        //  doc.addImage(imgData, 'PNG', -6, position, imgWidth, imgHeight);
-        //  heightLeft -= pageHeight;
-        //}
         doc.save('sample.pdf');
       });
     },
-    colorChange: function (color){
-      const self = this;
-      self.canvas = document.querySelector("#drawing-canvas");
-      self.context = this.canvas.getContext('2d');
-      self.curColor = color;
-
-      self.context.strokeStyle = color;
-    },
-    rangeChange: function (range){
-      this.vidcanvas = document.querySelector("#videoCanvas");
-      this.vidcontext = this.canvas.getContext('2d');
-      this.canvas = document.querySelector("#drawing-canvas");
-      this.context = this.canvas.getContext('2d');
-      this.context.lineWidth = range;
-      this.vidcontext.lineWidth = range;
-      this.brushSize = range;
-    },
-    selectMode: function (mode){
-      if(mode==='select'||mode==='text') this.isPainting = false;
-      this.paintMode = mode;
-    },
-    canvasMousedown: function (){
-      if(!this.isPainting && (this.paintMode==='draw'||this.paintMode==='light') ) this.isPainting = true;
-    },
-    canvasMousemove: function (event){
-      const x = event.offsetX;
-      const y = event.offsetY;
-
-      const self = this;
-      self.canvas = document.querySelector("#drawing-canvas");
-      self.context = self.canvas.getContext('2d');
-      this.consize = document.querySelector('#canvas-container');
-      self.context.globalAlpha = 1;
-      self.context.lineWidth = this.brushSize;
-      console.log('canvas size : '+this.consize.clientWidth+' ,'+this.consize.clientHeight);
-
-      if(!this.isPainting){
-        self.context.beginPath();
-        self.context.moveTo(x,y);
-      }
-      else{
-        if(this.paintMode==='light'){
-          self.context.globalAlpha = 0.03;
-          self.context.lineWidth = self.brushSize*2;
-        }
-        // else if(this.paintMode==='erase'){
-        //   this.context.globalCompositeOperation = "destination-out";
-        //   this.context.strokeStyle = "rgba(0,0,0,1)";
-        //   console.log('erase');
-        // }
-        self.context.lineTo(x,y);
-        self.context.stroke();
-      }
-    },
-    stopPainting: function (){
-      this.canvas = document.querySelector("#drawing-canvas");
-      this.context = this.canvas.getContext('2d');
-      if(this.isPainting===true){
-        this.undoStack.push(this.context.getImageData(0,0,this.canvas.width,this.canvas.height));
-        this.redoStack.length=0;
-      }
-      this.isPainting = false;
-    },
-    undoExec: function (){
-      if (this.undoStack.length <= 1){
-        return;
-      }
-      this.canvas = document.querySelector("#drawing-canvas");
-      this.context = this.canvas.getContext('2d');
-      this.redoStack.push(this.undoStack[this.undoStack.length - 1]);
-      this.undoStack.pop();
-      this.context.putImageData(this.undoStack[this.undoStack.length - 1],0,0);
-    },
-    redoExec: function (){
-      if (this.redoStack.length < 1){
-        return;
-      }
-      this.canvas = document.querySelector("#drawing-canvas");
-      this.context = this.canvas.getContext('2d');
-      this.undoStack.push(this.redoStack[this.redoStack.length - 1]);
-      this.context.putImageData(this.redoStack[this.redoStack.length - 1],0,0);
-      this.redoStack.pop();
-    },
-    canvasClear: function (){
-      const originImg = document.createElement('img');
-      originImg.src = this.canvasImgsrc;
-      this.canvas = document.querySelector("#drawing-canvas");
-      this.context = this.canvas.getContext('2d');
-
-      this.context.drawImage(originImg, 0, 0, this.canvas.width, this.canvas.height);
-      this.undoStack.push(this.context.getImageData(0,0,this.canvas.width,this.canvas.height));
-    },
     popupCanvas: function (event){
-      document.getElementById('dimmed').classList.remove('hidden');
-      document.getElementById('canvas-container').classList.remove('hidden');
-      this.canvas = document.getElementById('drawing-canvas');
-      this.context = this.canvas.getContext('2d');
-      const printImg = document.createElement('img');
+      this.video = document.querySelector("#videoOrigin");
 
       this.canvasImgsrc = event.target.src;
-      printImg.src = event.target.src;
-      this.context.drawImage(printImg, 0, 0, this.canvas.width, this.canvas.height);
-      this.undoStack.push(this.context.getImageData(0,0,this.canvas.width,this.canvas.height));
-
-      this.context.strokeStyle = this.curColor;
-      this.context.lineWidth = this.brushSize;
-      console.log('canvas wid, hei '+this.canvas.width+' '+this.canvas.height);
+      this.isCanvasOn = true;
+      this.$refs.drawingPopup.drawingImage(event.target.src,this.video.clientWidth,this.video.clientHeight);
     },
-    closePopup: function (){
-      document.getElementById('dimmed').classList.add('hidden');
-      document.getElementById('canvas-container').classList.add('hidden');
-      this.undoStack.length = 0;
-      this.redoStack.length = 0;
-    }
   }
 }
 </script>
 
 <style scoped>
-.comment-box{
+.note-box{
   height: 600px;
 }
 .video-frame{
@@ -450,110 +319,6 @@ export default {
   padding: 10px;
   border: 1px solid;
   overflow-y: auto;
-}
-.edit-toolbar{
-  margin-bottom: 10px;
-  margin-top : 10px;
-}
-#commentArea {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-.owner .ownerAvatar > a > img {
-  width: 40px;
-  height: 40px;
-  border-radius: 100%;
-}
-.comment {
-  display: flex;
-  padding: 5px 10px;
-  margin-bottom: 10px;
-  align-items: center;
-  color: #333;
-  background-color: #F2F2F2;
-  border-radius: 30px;
-  box-shadow: 1px 1px 3px rgba(0, 0, 0, 0.2);
-}
-.comment .username {
-  align-self: flex-start;
-  margin-top: 5px;
-}
-.comment .avatar > a > img {
-  width: 40px;
-  height: 40px;
-  border-radius: 100%;
-  align-self: start;
-}
-.comment .user{
-  text-align: left;
-  margin-left: 5px;
-}
-.comment .texts {
-  text-align: left;
-  margin-left: 15px;
-}
-.comment .time {
-  text-align: left;
-  margin-left: 20px;
-}
-.comment .inlineBtn {
-  display: inline;
-}
-.custom-scrollbar{
-  max-height: 250px;
-  overflow-y: auto;
-  padding-right: 10px;
-}
-.custom-scrollbar::-webkit-scrollbar-track
-{
-  -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
-  -moz-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
-  box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
-  border-radius: 10px;
-  background-color: #fff;
-}
-.custom-scrollbar::-webkit-scrollbar
-{
-  width: 8px;
-  background-color: #fff;
-}
-.custom-scrollbar::-webkit-scrollbar-thumb
-{
-  border-radius: 10px;
-  -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,.3);
-  -moz-box-shadow: inset 0 0 6px rgba(0,0,0,.3);
-  box-shadow: inset 0 0 6px rgba(0,0,0,.3);
-  background-color: #555;
-}
-#dimmed{
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: #000;
-  opacity: .8;
-  z-index: 10;
-}
-#canvas-container{
-  position: absolute;
-  top : auto;
-  left: auto;
-  width: 80%;
-  height: auto;
-  padding: 1%;
-  background-color: white;
-  z-index: 100;
-}
-#drawing-canvas{
-  /*width: 80%;*/
-  /*height: auto;*/
-  background-color: dimgrey;
-  object-fit: cover;
 }
 .hidden{
   display: none;
