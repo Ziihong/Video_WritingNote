2ee8fef146abd5df198a755de8ecbdef748be640
Lee <inhoking@kookmin.ac.kr>
2021-07-22 12:08:30 +0900

    FIX: Video&Voice call

--- components/Webcam.vue
+++ components/Webcam.vue
@@ -1,23 +1,24 @@
 <template>
   <div>
     <div class='informBox'>
-      <div class="input-field" style="margin: 10px">
-        <label>User ID</label>
-        <input type="text" placeholder="User ID" id="userID">
-      </div>
       <v-row>
         <div style="margin: 10px">
-          <v-btn color="primary" id="login" @click="loginUser"
+          <v-btn color="primary" id="login" @click="loginUser(authUser.nickname)"
           >LOGIN</v-btn>
           <v-btn type="button" id="logout" @click="logoutUser"
           >LOGOUT</v-btn>
         </div>
       </v-row>
       <div class="agora-button">
-        <v-btn type="primary" @click="joinEvent">join</v-btn>
-        <v-btn type="primary" @click='leaveEvent'>leave</v-btn>
+        <v-btn v-if="!onVideo" type="primary" @click="videoCallJoin"><v-icon left>mdi-video</v-icon></v-btn>
+        <v-btn v-else @click='leaveChannel'><v-icon>mdi-video-off</v-icon></v-btn>
+        <v-btn v-if="!onVoice" type="primary" @click="voiceCallJoin"><v-icon left>mdi-phone</v-icon></v-btn>
+        <v-btn v-else @click='leaveChannel'><v-icon>mdi-phone-off</v-icon></v-btn>
       </div>
     </div>
+    <template v-for="(member) of userList">
+      <li>{{member}}</li>
+    </template>
     <div class="agora-view">
       <div class="showVideo">
         <StreamPlayer :stream="localStream" :domId="localStream.getId()" v-if="localStream"></StreamPlayer>
@@ -34,18 +35,25 @@ import StreamPlayer from "../components/StreamPlayer";
 import AgoraRTC from "agora-rtc-sdk";
 import {RtmRole, RtmTokenBuilder} from "agora-access-token";
 import AgoraRTM from "agora-rtm-sdk";
+import {mapGetters, mapState} from "vuex";
 
 export default {
   components: {
     StreamPlayer
   },
+  computed: {
+    ...mapState({
+      authUser: state => state.authUser,
+    }),
+    ...mapGetters({
+      isLoggedIn: 'isLoggedIn',
+    }),
+  },
   data () {
     return {
       option: {
-        appid: '',
         token: '',
-        uid: null,
-        channel: '',
+        uid: '',
       },
       disableJoin: false,
       localStream: null,
@@ -56,63 +64,50 @@ export default {
       channel: "",
       appId: "",
       appCertificate: "",
+      userList: [],
+      onVideo: false,
+      onVoice: false,
     }
   },
   created() {
+    const self = this;
     this.appId = "e68902b5adbf4686abbf25626ba75b91";
     this.appCertificate = "8789fcbdc5514adbb59cbc42f17ee7fb";
     this.clientID = AgoraRTM.createInstance(this.appId);
     this.channel = this.clientID.createChannel("demoChannel");
+    this.channel.on('MemberJoined', function (memberId) {
+      self.userList.push(memberId);
+    })
+    this.channel.on('MemberLeft', function (memberId) {
+      self.userList = self.userList.filter((member)=>member!=memberId);
+    })
+  },
+  mounted() {
+    this.channel.getMembers().then((memberList)=>{
+      this.userList = memberList;
+    });
   },
   methods: {
-    async loginUser(){
-      this.option.uid = document.getElementById("userID").value.toString();
+    async loginUser(nickname){
+      this.option.uid = nickname;
       this.option.token = this.tokenGenerate(this.option.uid);
+      alert("Success agora logIn");
       console.log('RTM Tokens : '+this.option.token);
       await this.clientID.login(this.option).catch(function (err){
         console.log('AgoraRTM client login failure!!!');
       });
     },
     async logoutUser(){
+      alert("Success agora logOut");
       await this.clientID.logout().catch(function (err){
         console.log('AgoraRTM client logout failure!!!');
       });
     },
-    joinEvent () {
-      this.joinChannel(this.option).then(() => {
-        alert("Join Success");
-
-        this.publishStream().then((stream) => {
-          alert("Publish Success");
-          this.localStream = stream
-        }).catch((e) => {
-          alert("Publish Failure");
-          console.log(e)
-        })
-      }).catch((err) => {
-        alert("Join Failure");
-      })
-      this.disableJoin = true
-    },
-    leaveEvent () {
-      this.disableJoin = false
-      this.leaveChannel().then(() => {
-        alert("Leave Success");
-      }).catch((e) => {
-        alert("Leave Failure");
-      })
-      this.localStream = null
-      this.remoteStreams = []
-    },
-    judge(detail) {
-      alert(`Please enter the ${detail}`);
-    },
     async joinChannel(){
       await this.channel.join().then(()=>{
-        document.getElementById("log").appendChild(document.createElement('div'))
-          .append("You have successfully joined channel " + this.channel.channelId)
+        alert("Join Success");
       }).catch(function (err){
-        console.log('AgoraRTM channel join failure!!!');
+        alert("Join Failure");
       });
       this.channel.getMembers().then((memberList)=>{
         this.userList = memberList;
@@ -122,25 +117,32 @@ export default {
     async leaveChannel(){
       if (this.channel != null) {
         await this.channel.leave().catch(function (err){
+          alert("Leave Failure");
           console.log('Channel leaving failed!');
         });
-        this.userList = this.userList.filter((member)=>member!=this.options.uid);
+        alert("Leave Success");
+        this.disableJoin = false
+        this.localStream = null;
+        this.remoteStreams = [];
+        this.userList = this.userList.filter((member)=>member!=this.option.uid);
+        this.onVideo = false;
+        this.onVoice = false;
       }
       else
       {
         console.log("Channel is empty");
       }
     },
-    publishStream() {
-      return new Promise((resolve, reject) => {
-        // Create a local stream
+    publishVideoCall() {
+      return new Promise((resolve) => {
+        // Create a videoStream
         this.stream = AgoraRTC.createStream({
           streamID: this.option.uid,
           audio: true,
           video: true,
           screen: false,
         })
-        // Initialize the local stream
+        // Initialize the stream
         this.stream.init(() => {
           console.log("init local stream success")
           resolve(this.stream)
@@ -152,16 +154,16 @@ export default {
         })
       })
     },
-    publishVoiceStream() {
-      return new Promise((resolve, reject) => {
-        // Create a local stream
+    publishVoiceCall() {
+      return new Promise((resolve) => {
+        // Create a voiceStream
         this.stream = AgoraRTC.createStream({
           streamID: this.option.uid,
           audio: true,
           video: false,
           screen: false
         })
-        // Initialize the local stream
+        // Initialize the stream
         this.stream.init(() => {
           console.log("init local stream success")
           resolve(this.stream)
@@ -172,6 +174,36 @@ export default {
           })
         })
       })
+    },
+    videoCallJoin () {
+      this.joinChannel().then(() => {
+        this.publishVideoCall().then((stream) => {
+          alert("Publish Success");
+          this.localStream = stream
+          this.onVideo = true;
+        }).catch((e) => {
+          alert("Publish Failure");
+          console.log(e)
+        })
+      }).catch((err) => {
+        alert("Join Failure");
+      })
+      this.disableJoin = true
+    },
+    voiceCallJoin (){
+      this.joinChannel().then(() => {
+        this.publishVoiceCall().then((stream) => {
+          alert("Publish Success");
+          this.localStream = stream
+          this.onVoice = true;
+        }).catch((e) => {
+          alert("Publish Failure");
+          console.log(e)
+        })
+      }).catch((err) => {
+        alert("Join Failure");
+      })
+      this.disableJoin = true
     },
     tokenGenerate(account){
       const expirationTimeInSeconds = 7200;
